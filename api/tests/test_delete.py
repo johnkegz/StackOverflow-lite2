4ecfafba7e5e7f6873b4fcf01f2dd8a85f748473
johnkegz <johnkl24@gmail.com>
2018-08-30 10:22:02 -0700

    Feature(Delete question): Delete question from table of questions[#160046040]

---
+++ api/tests/test_delete.py
@@ -0,0 +1,27 @@
+"""
+    Module for making tests on the app for sign up
+"""
+import unittest
+import json
+from run import app
+class TestViews(unittest.TestCase):
+    """"
+        Class for making tests on sign up
+        params: unittest.testCase
+    """
+
+    def setUp(self):
+        """
+           Method for making the client object
+        """
+        self.client = app.test_client
+    def test_sign(self):
+        """
+            Method for tesing the post function which posts a adds a new user
+        """
+        result = self.client().delete('/Delete /questions/15')
+        respond = json.loads(result.data.decode("utf8"))
+        self.assertIn('Delete response', respond)
+        self.assertIsInstance(respond, dict)
+        self.assertEqual(result.status_code, 200)
+        self.assertTrue(result.json["Delete response"])
