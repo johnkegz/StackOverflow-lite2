4b84ec33ae5efb41e295e0d623f126b1d76a52f6
johnkegz <johnkl24@gmail.com>
2018-08-30 21:30:35 -0700

    Chore(JWT):Add jwt to secure end points[#160046069]

--- run.py
+++ run.py
@@ -2,12 +2,13 @@
    Module for starting/ running the app
 """
 from flask import Flask
-from routes.routes import GetRoutes
+from api.routes.routes import GetRoutes
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 GetRoutes.fetch_routes(app)
+
 app.config['JWT_SECRET_KEY'] = 'kjdkhfdfi'
 jwt = JWTManager(app)
 
