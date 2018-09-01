16684d555e426936f353b419f96194b93f7fbc8f
johnkegz <johnkl24@gmail.com>
2018-09-01 07:14:18 -0700

    Chore(pep8): run pylint to follow pep8 stndards[#160215136]

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,8 +1,8 @@
 """
    Class for defining url routes
 """
-from api.controllers.views import Login, GetQuestion, SignUp, NewQuestion, NewAnswer, DeleteQuestion, AcceptAnswer
-
+from api.controllers.views import Login, GetQuestion, SignUp
+from api.controllers.views import NewQuestion, NewAnswer, DeleteQuestion, AcceptAnswer
 
 class GetRoutes():
     """
@@ -14,11 +14,21 @@ class GetRoutes():
         """
            Add url rule defines the routes for http requests
         """
-        url.add_url_rule('/api/v1/auth/signup', view_func=SignUp.as_view('Signup'), methods=['POST',])
-        url.add_url_rule('/api/v1/auth/login', view_func=Login.as_view('Login'), methods=['POST',])
-        url.add_url_rule('/api/v1/questions', view_func=GetQuestion.as_view('questions'), defaults={'question_id': None}, methods=['GET',])
-        url.add_url_rule('/api/v1/questions/<int:question_id>', view_func=GetQuestion.as_view('one_questions'), methods=['Get',])
-        url.add_url_rule('/api/v1/questions', view_func=NewQuestion.as_view('New question'), methods=['POST',])
-        url.add_url_rule('/api/v1/questions/<question_id>/answers', view_func=NewAnswer.as_view('New answer'), methods=['POST',])
-        url.add_url_rule('/api/v1/Delete /questions/<question_id>', view_func=DeleteQuestion.as_view('Delete Question'), methods=['DELETE',])
-        url.add_url_rule('/api/v1/questions/<question_id>/answers/<answer_id>', view_func=AcceptAnswer.as_view('Accept answer'), methods=['PUT'])
+        url.add_url_rule('/api/v1/auth/signup',
+                         view_func=SignUp.as_view('Signup'), methods=['POST',])
+        url.add_url_rule('/api/v1/auth/login',
+                         view_func=Login.as_view('Login'), methods=['POST',])
+        url.add_url_rule('/api/v1/questions',
+                         view_func=GetQuestion.as_view('questions'),
+                         defaults={'question_id': None}, methods=['GET',])
+        url.add_url_rule('/api/v1/questions/<int:question_id>',
+                         view_func=GetQuestion.as_view('one_questions'), methods=['Get',])
+        url.add_url_rule('/api/v1/questions',
+                         view_func=NewQuestion.as_view('New question'), methods=['POST',])
+        url.add_url_rule('/api/v1/questions/<question_id>/answers',
+                         view_func=NewAnswer.as_view('New answer'), methods=['POST',])
+        url.add_url_rule('/api/v1/Delete /questions/<question_id>',
+                         view_func=DeleteQuestion.as_view('Delete Question'), methods=['DELETE',])
+        url.add_url_rule('/api/v1/questions/<question_id>/answers/<answer_id>',
+                         view_func=AcceptAnswer.as_view('Accept answer'), methods=['PUT'])
+                         
